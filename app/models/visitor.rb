9b970fed6e9d54a795ce8bf198f453d31217f944
Kate Flood <katefloodster@gmail.com>
2014-03-25 20:11:45 -0700

    small fixes

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -2,7 +2,7 @@ class Visitor < ActiveRecord::Base
   has_no_table
   column :email, :string
   validates_presence_of :email
-  validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-zo-9]{2,4}\z/if condition
+  validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
   
   def subscribe
     mailchimp = Gibbon::API.new
